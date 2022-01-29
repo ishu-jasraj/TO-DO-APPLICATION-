d6897224d9858c5199447a4ce1bd8e079a29ef96
ishu-jasraj <91381940+ishu-jasraj@users.noreply.github.com>
2022-01-29 21:25:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@
 const yargs = require('yargs')
 const notes = require('./notes.js')
 
-// Customize yargs version
-yargs.version('1.1.0')
-debugger
 // Create add command
 yargs.command({
     command: 'add',
